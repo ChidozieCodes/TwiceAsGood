5011198d67996d71f92b1bd28d3e3e3174f5ceb8
unknown <iwuanyanwuaugustine@gmail.com>
2024-07-07 18:44:36 +0100

    Your descriptive commit message here

---
+++ user/footer.php
@@ -0,0 +1,57 @@
+<footer>
+        <article id="footer_inner">
+            <section id="footer_sec1">
+                <h2>TwiceAsGood</h2><br><br>
+                <p>
+                    Where every game is a lesson, and every lesson is a game.
+                    Our app transforms education into playful games, captivating
+                    young minds with interactive fun.
+                </p><br><br>
+                <ul>
+                    <li><img src="img/apple_store.png" alt=""></li>
+                    <li><img src="img/google_play.png" alt=""></li>
+                </ul>
+            </section>
+            <section id="footer_sec2">
+                <div>
+                    <h2>Quick Links</h2><br>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Courses</a></li>
+                        <li><a href="">E-books</a></li>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">About</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Company</h2><br>
+                    <ul>
+                        <li><a href="">Contact</a></li>
+                        <li><a href="">Help Center</a></li>
+                        <li><a href="">Reviews</a></li>
+                        <li><a href="">FAQS</a></li>
+                        <li><a href="">Blog</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Connect with us</h2><br>
+                    <ul>
+                        <li><a href="">Facebook</a></li>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">Twitter</a></li>
+                        <li><a href="">Tiktok</a></li>
+                    </ul>
+                </div>
+            </section>
+        </article>
+        <article id="footer_inner2">
+            <ul>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Terms & Conditions</a></li>
+                <li><a href="">Support</a></li>
+            </ul>
+            <p>© Copyright 2024, All Rights Reserved</p>
+        </article>
+    </footer>
+</body>
+</html>
---
+++ user/index.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'?>
+    <section id="main_aside_wrapper">
+        <aside></aside>
+        <main>
+            <article id="dashbanner">
+                <img src="img/dashbanner.png" alt="">
+            </article>
+        </main>
+    </section>
+<?php include 'footer.php'?>
